c464f62f65b37470a0540029ab8135a845f0f824
Yash Singh Pathania <newtoneinstienfarady@gmail.com>
2024-10-07 16:59:02 +0100

    I think task 3 is now done for sure sure

--- task3/girlsallowed/src/main/java/com/student24204265/girlsallowed/Main.java
+++ task3/girlsallowed/src/main/java/com/student24204265/girlsallowed/Main.java
@@ -1,29 +1,35 @@
-import java.rmi.registry.*;
+package com.student24204265.girlsallowed;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import service.dodgygeezers.DGQService;
-import service.core.QuotationService;
+
 import service.core.Constants;
+import service.core.QuotationService;
+import service.dodgygeezers.DGQService;
 
 public class Main {
 
     public static void main(String[] args) {
         QuotationService dgqService = new DGQService();
         try {
-            Registry registry;
-
+            // Connect to the RMI Registry - creating the registry will be the responsibility of the broker.
+            Registry registry = null;
             if (args.length == 0) {
                 registry = LocateRegistry.createRegistry(1099);
-                System.out.println("New RMI Registry created at port 1099");
             } else {
                 registry = LocateRegistry.getRegistry(args[0], 1099);
-                System.out.println("Connected to existing RMI Registry at " + args[0]);
             }
 
+            // Create the Remote Object
             QuotationService quotationService = (QuotationService) UnicastRemoteObject.exportObject(dgqService, 0);
 
+            // Register the object with the RMI Registry
             registry.bind(Constants.DODGY_GEEZERS_SERVICE, quotationService);
-            System.out.println("Dodgy Geezers Service is running and bound to the RMI Registry...");
+            System.out.println("Dodgy Geezers Service bound successfully");
 
+            // Keep the server alive
+            System.out.println("STOPPING SERVER SHUTDOWN");
             while (true) {
                 Thread.sleep(1000);
             }
