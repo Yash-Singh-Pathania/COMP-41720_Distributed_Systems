618e401cc4483781b1d324bb70747bb3f8a022e0
Yash Singh Pathania <newtoneinstienfarady@gmail.com>
2024-10-07 16:19:57 +0100

    I think task 3 is done

--- task3/auldfellas/src/main/java/com/student24204265/auldfellas/Main.java
+++ task3/auldfellas/src/main/java/com/student24204265/auldfellas/Main.java
@@ -14,7 +14,12 @@ public class Main {
         QuotationService afqService = new AFQService();
         try {
             // Connect to the RMI Registry - creating the registry will be the responsibility of the broker.
-            Registry registry = LocateRegistry.createRegistry(1099);
+            Registry registry = null;
+            if (args.length == 0) {
+                registry = LocateRegistry.createRegistry(1099);
+            } else {
+                registry = LocateRegistry.getRegistry(args[0], 1099);
+            }
 
             // Create the Remote Object
             QuotationService quotationService = (QuotationService) UnicastRemoteObject.exportObject(afqService, 0);
